4742225d3d0c32f4142a8c900a17e9cd5f276c85
Thorsten Kober <info@flashdesignory.com>
2018-04-21 19:24:26 -0700

    changed cors method to simpler implementation

--- src/server.js
+++ src/server.js
@@ -11,7 +11,15 @@ const app = express();
 app.locals.title = 'express server';
 
 //alllow access to api
-app.use(cors())
+//app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 //parse json payloads
 app.use(express.json());
