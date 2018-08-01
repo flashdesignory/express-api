ac35f5614771800229dc780752239e5c64d7a51f
Thorsten Kober <info@flashdesignory.com>
2018-08-01 13:32:35 -0700

    testing

--- src/db.js
+++ src/db.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
-const connect = () => mongoose.connect(
-  process.env.DATA_CONNECTION,
-  { useNewUrlParser: true },
-);
+const connect = () => {
+  console.log("url: " + process.env.DATA_CONNECTION);
+  mongoose.connect(
+    process.env.DATA_CONNECTION,
+    { useNewUrlParser: true },
+  )
+};
 
 export default connect;
